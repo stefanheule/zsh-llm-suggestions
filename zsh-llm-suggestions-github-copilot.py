cef91e4bd65fc0ff4f1fe3bfd0ced358260ae83b
Stefan Heule <stefanheule@gmail.com>
2023-12-09 16:09:45 -0800

    work in progress

--- zsh-llm-suggestions-github-copilot.py
+++ zsh-llm-suggestions-github-copilot.py
@@ -9,12 +9,12 @@ MISSING_PREREQUISITES = "zsh-llm-suggestions missing prerequisites:"
 
 def main():
 
-  if not os.path.isfile('/usr/bin/gh'):
+  if subprocess.run(['gh', 'version'], text=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL).returncode != 0:
     print(f'echo "{MISSING_PREREQUISITES} Install GitHub CLI first by following https://github.com/cli/cli#installation"')
     return 0
   
   buffer = sys.stdin.read()
-  process = subprocess.Popen(['/usr/bin/gh', 'copilot', 'suggest', '-t', 'shell', buffer], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+  process = subprocess.Popen(['gh', 'copilot', 'suggest', '-t', 'shell', buffer], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
   
   output, error = process.communicate()
 
@@ -33,14 +33,14 @@ def main():
   output = ansi_escape.sub('', output)
 
   if "Error: No valid OAuth token detected" in error:
-    print(f"echo '{MISSING_PREREQUISITES} Authenticate with github first:' && /usr/bin/gh auth login --web -h github.com")
+    print(f"echo '{MISSING_PREREQUISITES} Authenticate with github first:' && \gh auth login --web -h github.com")
     return 0
 
   if 'unknown command "copilot" for "gh"' in error:
-    if "You are not logged into any GitHub hosts" in subprocess.run(['/usr/bin/gh', 'auth', 'status'], text=True, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL).stderr:
-      print(f"echo '{MISSING_PREREQUISITES} Authenticate with github first:' && /usr/bin/gh auth login --web -h github.com")
+    if "You are not logged into any GitHub hosts" in subprocess.run(['gh', 'auth', 'status'], text=True, stderr=subprocess.PIPE, stdout=subprocess.DEVNULL).stderr:
+      print(f"echo '{MISSING_PREREQUISITES} Authenticate with github first:' && \gh auth login --web -h github.com")
       return 0
-    print(f"echo '{MISSING_PREREQUISITES} Install github copilot extension first:' && /usr/bin/gh extension install github/gh-copilot")
+    print(f"echo '{MISSING_PREREQUISITES} Install github copilot extension first:' && \gh extension install github/gh-copilot")
     return 0
 
   if "Suggestion not readily available. Please revise for better results." in output:
